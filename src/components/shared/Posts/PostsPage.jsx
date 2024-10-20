eabe99ea61594ccca3873388452c22542f9f0a2c
m4tiss <mateusz039@op.pl>
2024-10-20 18:59:24 +0200

    add RunPostPanel

--- src/components/shared/Posts/PostsPage.jsx
+++ src/components/shared/Posts/PostsPage.jsx
@@ -1,51 +1,49 @@
 import axios from "../../../config/axios";
 import { getAuthToken } from "../../../config/auth";
 import { useState, useEffect } from "react";
+import { TbRun } from "react-icons/tb";
+import { CgGym } from "react-icons/cg";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
-import RunPostPanel from "./RunPostPanel"
-import GymPostPanel from "./GymPostPanel"
+import RunPostPanel from "./RunPostPanel";
+import GymPostPanel from "./GymPostPanel";
 
 const PostsPage = () => {
+  const [posts, setPosts] = useState([]);
 
-  const [posts,setPosts] = useState([]);
+  // const sendPost = async () => {
+  //   try {
+  //     const token = getAuthToken();
+  //     const response = await axios.post(
+  //       "/shared/post",
+  //       {
+  //         gym_workout_id: null,
+  //         run_workout_id: 82,
+  //         description: "Opis treningu biegowego",
+  //       },
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
 
-    // const sendPost = async () => {
-    //   try {
-    //     const token = getAuthToken();
-    //     const response = await axios.post(
-    //       "/shared/post",
-    //       {
-    //         gym_workout_id: null,
-    //         run_workout_id: 82,
-    //         description: "Opis treningu biegowego",
-    //       },
-    //       {
-    //         headers: {
-    //           Authorization: `Bearer ${token}`,
-    //         },
-    //       }
-    //     );
-
-    //     console.log("Post dodany pomyślnie:", response.data);
-    //   } catch (error) {
-    //     console.error("Błąd podczas dodawania posta:", error);
-    //   }
-    // };
+  //     console.log("Post dodany pomyślnie:", response.data);
+  //   } catch (error) {
+  //     console.error("Błąd podczas dodawania posta:", error);
+  //   }
+  // };
 
   useEffect(() => {
     const getAllPosts = async () => {
       try {
         const token = getAuthToken();
-        const response = await axios.get(
-          "/shared/post/getAll",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setPosts(response.data)
+        const response = await axios.get("/shared/post/getAll", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setPosts(response.data);
       } catch (error) {
         console.error("Błąd podczas dodawania posta:", error);
       }
@@ -55,11 +53,31 @@ const PostsPage = () => {
   }, []);
 
   return (
-    <div className="w-full flex flex-grow flex-col justify-start items-center bg-[#e9ecef] dark:bg-run-night-background py-10 xl:px-0 px-5 gap-10">
+    <div className="w-full flex flex-grow flex-col justify-start items-center bg-[#e9ecef] dark:bg-run-night-background py-10 xl:px-0 px-5 gap-8">
+      <div className="flex gap-10">
+        <div
+          style={{
+            backgroundImage:
+              "linear-gradient(to bottom, #e73725, #e62c37, #e22547, #dd2155, #d52362)",
+          }}
+          className="h-12 w-36 flex text-white text-2xl justify-center items-center gap-3 rounded-xl"
+        >
+          NEW <CgGym size={40}/>
+        </div>
+        <div
+          style={{
+            backgroundImage:
+              "linear-gradient(to bottom, #1da1f2, #1794e4, #1087d5, #087ac7, #006eb9)",
+          }}
+          className="h-12 w-36 flex text-white  text-2xl justify-center items-center gap-3 rounded-xl"
+        >
+          NEW <TbRun size={40}/>
+        </div>
+      </div>
       {posts.map((post) => {
-        if (post.type === 'run') {
+        if (post.type === "run") {
           return <RunPostPanel key={post.post_id} post={post} />;
-        } else if (post.type === 'gym') {
+        } else if (post.type === "gym") {
           return <GymPostPanel key={post.post_id} post={post} />;
         }
         return null;
--- src/components/shared/Posts/RunPostPanel.jsx
+++ src/components/shared/Posts/RunPostPanel.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import { CiHeart } from "react-icons/ci";
 import { GoComment } from "react-icons/go";
 import { RiShareBoxFill } from "react-icons/ri";
+import { motion } from "framer-motion";
+import {
+  formattedData,
+  formattedTime,
+  formattedDate,
+  formattedDuration,
+} from "../../../functions/formatData";
+import ReactStars from "react-stars";
+import { useContext } from "react";
+import DarkModeContext from "../../../providers/DarkModeProvider";
+import iconDark from "../../../icons/runIconDark.png";
+import iconLight from "../../../icons/runIconLight.png";
+import { LuMapPin } from "react-icons/lu";
+import { LuMapPinOff } from "react-icons/lu";
 
 const RunPostPanel = ({ post }) => {
+  const { darkMode } = useContext(DarkModeContext);
+  const [isLiked, setIsLiked] = useState(false);
   console.log(post);
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="w-1/2 h-80 flex bg-white p-5 rounded-xl">
       <div className="w-1/2 flex flex-col gap-5">
@@ -23,13 +44,67 @@ const RunPostPanel = ({ post }) => {
           {post?.description}
         </div>
         <div className="flex gap-5 items-center px-10">
-          <CiHeart size={35} />
-          <GoComment size={25} />
-          <RiShareBoxFill size={25} />
+          <div className="cursor-pointer" onClick={handleLike}>
+            <motion.div transition={{ type: "spring", stiffness: 300 }}>
+              <CiHeart size={35} color={isLiked ? "red" : "black"} />
+            </motion.div>
+          </div>
+          <div>
+            <GoComment size={25} />
+          </div>
+          <div>
+            <RiShareBoxFill size={25} />
+          </div>
         </div>
       </div>
 
-      <div className="w-1/2"></div>
+      <div className="w-1/2 flex flex-col justify-evenly items-center gap-3">
+        <div className="flex justify-center items-center gap-10">
+          <h2 className="text-2xl">
+            {post?.workout.category_name || "UNKNOWN TITLE"}
+          </h2>
+          <img
+            width={60}
+            src={darkMode ? iconDark : iconLight}
+            alt="Run Icon"
+          />
+        </div>
+        <div className="flex justify-start w-full px-5  text-xl">
+          <span className="text-5xl text-center w-full">
+            {formattedData(post?.workout.distance || 0)} km
+          </span>
+        </div>
+        <div className="flex items-center justify-evenly w-full px-5 text-xl">
+          <span className="text-center">
+            {formattedDuration(post?.workout.duration || "00:00:00")}
+          </span>
+          <div>
+            {post?.workout.coordinates.length > 0 ? (
+              <LuMapPin size={40} />
+            ) : (
+              <LuMapPinOff size={40} />
+            )}
+          </div>
+        </div>
+
+        <div className="w-full flex justify-evenly items-center">
+          <h2 className="text-3xl">{formattedData(post?.workout.rating || 0)}</h2>
+          <ReactStars
+            count={5}
+            size={30}
+            color1="gray"
+            color2={"#ffd700"}
+            value={post?.workout.rating}
+            edit={false}
+          />
+        </div>
+        <div className="w-full flex justify-evenly items-center">
+
+            <h2 className="text-xl">{formattedTime(post?.workout.date)}</h2>
+            <h2 className="text-xl">{formattedDate(post?.workout.date)}</h2>
+
+        </div>
+      </div>
     </div>
   );
 };
