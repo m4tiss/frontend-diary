1f1203e261e829b1fb0c12ac8be25f5f4f7d6450
m4tiss <mateusz039@op.pl>
2024-11-08 22:24:48 +0100

    add getting achievements for social and gym, basic layout

--- src/components/gym/GymAchievements.jsx/GymAchievements.jsx
+++ src/components/gym/GymAchievements.jsx/GymAchievements.jsx
@@ -1,10 +1,68 @@
+import AchievementPanel from "../../shared/AchievementPanel";
+import { useState, useEffect } from "react";
+import { getAuthToken } from "../../../config/auth";
+import axios from "../../../config/axios";
+
 const GymAchievements = () => {
+  const [gymAchievements, setGymAchievements] = useState([]);
+  const [socialAchievements, setSocialAchievements] = useState([]);
 
-    return (
-        <div className="w-full flex flex-col 2xl:flex-row flex-grow justify-evenly items-center bg-[#e9ecef] dark:bg-run-night-background py-10 xl:py-0 gap-10 2xl:gap-0">
-Gym
-        </div>
-      );
+  useEffect(() => {
+    const token = getAuthToken();
+    axios
+      .get(`/gym/achievement/getAll`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        const respone = res.data.achievements;
+        setGymAchievements(respone);
+      })
+      .catch((error) => {
+        console.error("Error fetching achievements data:", error);
+      });
+  }, []);
 
-}
+  useEffect(() => {
+    const token = getAuthToken();
+    axios
+      .get(`/shared/achievement/getAll`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        const respone = res.data.achievements;
+        console.log(respone);
+        setSocialAchievements(respone);
+      })
+      .catch((error) => {
+        console.error("Error fetching achievements data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="w-full flex flex-col flex-grow justify-evenly items-center bg-[#e9ecef] dark:bg-run-night-background py-10  gap-6">
+      <h2 className="text-3xl my-2">Gym Achievements</h2>
+      <div className="flex flex-wrap gap-8 py-20 justify-center">
+        {gymAchievements.map((achievement) => (
+          <AchievementPanel
+            key={achievement.users_gym_achievement_id}
+            achievement={achievement}
+          />
+        ))}
+      </div>
+      <h2 className="text-3xl my-2">Social Achievements</h2>
+      <div className="flex flex-wrap gap-8 py-20 justify-center">
+      {socialAchievements.map((achievement) => (
+          <AchievementPanel
+            key={achievement.users_social_achievement_id}
+            achievement={achievement}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default GymAchievements;
--- src/components/shared/AchievementPanel.jsx
+++ src/components/shared/AchievementPanel.jsx
@@ -1,28 +1,46 @@
 import { Line } from "rc-progress";
+import { GiWeight,GiGymBag } from "react-icons/gi";
+import { FaUserFriends } from "react-icons/fa";
+const AchievementPanel = ({ achievement }) => {
+  let icon;
+  if (achievement?.type === "workouts") {
+    icon = <GiWeight size={50} color="white" />;
+  } else if (achievement?.type === "sets") {
+    icon = <GiGymBag size={50} color="white"  />;
+  } else if (achievement?.type === "friends") {
+    icon = <FaUserFriends size={50} color="white"  />;
+  }
 
-const AcheviementPanel = ({ title, percent, description, setAchievement }) => {
+
+
   let lineColor = "#e63946";
-  if (percent > 30 && percent < 80) lineColor = "#ffd60a";
-  if (percent >= 80) lineColor = "#70e000";
+  if (achievement?.percent > 30 && achievement?.percent < 80) lineColor = "#ffd60a";
+  if (achievement?.percent >= 80) lineColor = "#70e000";
 
   return (
     <div
-      onClick={setAchievement}
-      className="w-96 h-64 flex flex-col items-center justify-evenly rounded-2xl
-           bg-white dark:bg-run-night-element dark:text-white border-black m-5 cursor-pointer shadow-xl"
+      className="w-96 h-64 flex flex-col items-center justify-start rounded-2xl
+           bg-white dark:bg-run-night-element dark:text-white border-black m-5 cursor-pointer relative py-10"
     >
-      <div className="w-full px-5 font-semibold text-right text-xl">
-        {percent}%
-        <Line percent={percent} trailColor="#FFFFFF" strokeColor={lineColor} />
+      <div
+        className="flex justify-center items-center w-20 h-20 rounded-xl shadow-xl absolute -top-10"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, #e73725, #e62c37, #e22547, #dd2155, #d52362)",
+        }}
+      >
+        {icon}
       </div>
-      
-        <div className="w-full flex items-center justify-center px-5 text-center flex-col">
-          <h2 className="text-2xl font-semibold">{title}</h2>
-          <span className="text-xl text-wrap">{description}</span>
-        </div>
-      
+      <div className="w-full flex items-center justify-center px-5 text-center flex-col my-2">
+        <h2 className="text-2xl font-semibold">{achievement?.title}</h2>
+        <span className="text-xl text-wrap">{achievement?.description}</span>
+      </div>
+      <div className="w-full px-5 font-semibold text-right text-xl">
+        {achievement?.percent}%
+        <Line percent={achievement?.percent} trailColor="#FFFFFF" strokeColor={lineColor} />
+      </div>
     </div>
   );
 };
 
-export default AcheviementPanel;
+export default AchievementPanel;
