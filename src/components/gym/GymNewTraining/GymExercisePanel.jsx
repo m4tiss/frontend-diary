1a47a0802abd60814fbb24eea55942b22ef28734
m4tiss <mateusz039@op.pl>
2024-12-07 19:34:40 +0100

    translation for exercises

--- src/components/gym/GymNewTraining/GymExercisePanel.jsx
+++ src/components/gym/GymNewTraining/GymExercisePanel.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-
+import { useTranslation } from "react-i18next";
 import chestIcon from '../../../icons/chestIcon.jpg';
 import backIcon from '../../../icons/backIcon.jpg';
 import bicepsIcon from '../../../icons/bicepsIcon.jpg';
@@ -20,7 +20,7 @@ const icons = {
 
 const GymExercisePanel = ({ height, exercise, onClick }) => {
 
-
+const { t } = useTranslation();
 const iconPath = icons[exercise.photo_exercise];
   return (
     <motion.div
@@ -30,10 +30,10 @@ const iconPath = icons[exercise.photo_exercise];
       whileTap={{ scale: 0.7 }}
       className={`rounded-2xl shadow-xl px-5 text-center flex items-center gap-5 justify-center bg-white h-${height} w-64 text-black cursor-pointer`}
     >
-      <h2>{exercise.name_exercise}</h2>
+      <h2>{t(`gym.exercises.${exercise.name_exercise}`)}</h2>
       <img
               src={iconPath}
-              alt={`${exercise.name_exercise} icon`}
+              alt={t(`gym.exercises.${exercise.name_exercise}Icon`)}
        height={70} width={70}/>
       
     </motion.div>
